e3d9e08c4f30905d7f21572a8ee7b4468bf47883
Yurii Rebryk <y.a.rebryk@gmail.com>
2019-04-04 17:18:13 -0700

    Bug fix

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,3 @@
 from .dataset import LJDataset
 from .post_dataset import PostDataset
 from .prepare_dataset import PrepareDataset
-from .utils import collate_fn_postnet
-from .utils import collate_fn_transformer
